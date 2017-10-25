9204eab139a6ebde47080f7d06d69a1870aa93ef
Nnalue Ugochukwu <31518858+ManCODEX@users.noreply.github.com>
2017-10-25 11:18:25 +0100

    Debugged and Tested

--- systeminfo.js
+++ systeminfo.js
@@ -1,49 +1,49 @@
-"use strict";
+ "use strict";
 
 // Master Class
-class laptop{
-    
-    constructor(screensize, webcam, opreratingSystem, batteryType){
+class laptopSystem{
+    constructor(screensize, webcam, operatingSystem, batteryType){
         this.screensize = screensize;
         this.webcam = webcam;
         this.operatingSystem = operatingSystem;
-        this.battertype = batterytype;
+        this.batterType = batteryType;
     }
     
 
-  laptopspecs(){
-        return ` your laptop specifications are \n 
+  systemspecs(){
+        return ` your system specifications are \n 
                 Screen Size: ${this.screensize} \n
-                webcam : ${this.webcam} \n
                 Operating Sytem: ${this.operatingSystem}\n
-                Operating Sytem: ${this.batterType}\n`             
+                Battery Type: ${this.batterType}\n`;             
     }
 }
 
    // Sub-Class
-class brandedSystem extends laptop{
+class BrandedSystem extends laptopSystem{
     
-    constructor ( brandname, processor, bodytype, chargingport, screensize, webcam, opreratingSystem, batteryType, ){
-        super(screensize, webcam, opreratingSystem, batteryType );
+    constructor ( brandname, processor, bodytype, chargingport,screensize, webcam, operatingSystem, batteryType){
+
+        super(screensize, webcam, operatingSystem, batteryType );
         this.brandname = brandname;
-        this.agproccessor = processor;
+        this.proccessor = processor;
         this.bodytype = bodytype;
         this.chargingport = chargingport;
         
     }
+
       //This Function adds a unique greeting to any brandname
     
-    laptopspecs(){
-        super.laptopspecs();
+    systemspecs(){
+        super.systemspecs();
     }
     
     greet(message){
         return message;
     }
     
+}
   
-            
-
-
-let Hp = new brandedSystem('Hp', 'octa-core','steel','usb-mode');
-hp.greet('Hello am HP i love .net Apps');
+     let Apple = new BrandedSystem('Apple', 'octa-core','stell','usb-mode','1650 by 750','1080Mpixels','ios','Leadpower');
+      console.log (Apple.greet('Hello am Apple and i am reliable and good for programmers,below are my specifications'));  
+       Apple;
+  
